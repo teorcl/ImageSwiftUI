5e45186466d06cffe4a1b18a277ec7b18e9695cd
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-08 11:10:41 -0500

    Initial Commit

---
+++ ImageSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageSwiftUI
+//
+//  Created by Teodoro Calle Lara on 8/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageSwiftUI/ImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSwiftUIApp.swift
+//  ImageSwiftUI
+//
+//  Created by Teodoro Calle Lara on 8/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
